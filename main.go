5e8ac0bccdb3a02f75fe3869857b5fc8f4e73767
Gildas Chabot <gildas.chabot@schibsted.com>
2018-06-16 14:48:40 +0200

    redirect to one of the 10 latest posts of user

--- main.go
+++ main.go
@@ -2,34 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
 	goinsta "gopkg.in/ahmdrz/goinsta.v2"
 )
 
-func getLatestPicture(i *goinsta.Instagram, username string) (string, error) {
+func getLatestPictures(i *goinsta.Instagram, username string, n int) ([]string, error) {
 	user, err := i.Profiles.ByName(username)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	media := user.Feed()
 
-	if !media.Next() {
-		return "", errors.Errorf("user %q have no post", username)
+	var urls []string
+
+	for media.Next() {
+		for _, i := range media.Items {
+			if len(i.Images.Versions) == 0 {
+				continue
+			}
+			urls = append(urls, i.Images.Versions[0].URL)
+			if len(urls) == n {
+				return urls, nil
+			}
+		}
 	}
 
-	if len(media.Items) <= 0 {
-		return "", errors.Errorf("user %q have no metia.Item", username)
+	return urls, nil
+}
+
+func oneOf(ss []string) string {
+	if len(ss) == 0 {
+		return ""
 	}
 
-	if len(media.Items[0].Images.Versions) <= 0 {
-		return "", errors.Errorf("user %q have no metia.Items[0].Images.Versions", username)
-	}
-
-	return media.Items[0].Images.Versions[0].URL, nil
+	return ss[rand.Intn(len(ss)-1)]
 }
 
 func main() {
@@ -40,14 +50,14 @@ func main() {
 	}
 
 	router := gin.Default()
-	router.GET("/instagram/:username/first.jpg", func(c *gin.Context) {
-		u, err := getLatestPicture(i, c.Param("username"))
+	router.GET("/instagram/:username/10.jpg", func(c *gin.Context) {
+		urls, err := getLatestPictures(i, c.Param("username"), 10)
 		if err != nil {
 			fmt.Println(err)
 			c.Status(http.StatusInternalServerError)
 			return
 		}
-		c.Redirect(http.StatusFound, u)
+		c.Redirect(http.StatusFound, oneOf(urls))
 	})
 
 	router.Run()
