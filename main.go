a8206b902aa5cdc5100623b605d5ee9633ca739f
Gildas Chabot <gildas.chabot@schibsted.com>
2018-06-21 19:25:13 +0200

    add route to get images from tag

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	goinsta "gopkg.in/ahmdrz/goinsta.v2"
 )
 
-func getLatestPictures(i *goinsta.Instagram, username string, n int) ([]string, error) {
+func getLatestPicturesFromUser(i *goinsta.Instagram, username string, n int) ([]string, error) {
 	user, err := i.Profiles.ByName(username)
 	if err != nil {
 		return nil, err
@@ -34,6 +34,27 @@ func getLatestPictures(i *goinsta.Instagram, username string, n int) ([]string, 
 	return urls, nil
 }
 
+func getLatestPicturesFromTag(i *goinsta.Instagram, tag string, n int) ([]string, error) {
+	feed, err := i.Search.FeedTags(tag)
+	if err != nil {
+		return nil, err
+	}
+
+	var urls []string
+
+	for _, i := range feed.Images {
+		if len(i.Images.Versions) == 0 {
+			continue
+		}
+		urls = append(urls, i.Images.Versions[0].URL)
+		if len(urls) == n {
+			return urls, nil
+		}
+	}
+
+	return urls, nil
+}
+
 func oneOf(ss []string) string {
 	if len(ss) == 0 {
 		return ""
@@ -50,8 +71,17 @@ func main() {
 	}
 
 	router := gin.Default()
-	router.GET("/instagram/:username/10.jpg", func(c *gin.Context) {
-		urls, err := getLatestPictures(i, c.Param("username"), 10)
+	router.GET("/instagram/user/:username/10.jpg", func(c *gin.Context) {
+		urls, err := getLatestPicturesFromUser(i, c.Param("username"), 10)
+		if err != nil {
+			fmt.Println(err)
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		c.Redirect(http.StatusFound, oneOf(urls))
+	})
+	router.GET("/instagram/tag/:tag/10.jpg", func(c *gin.Context) {
+		urls, err := getLatestPicturesFromTag(i, c.Param("tag"), 100)
 		if err != nil {
 			fmt.Println(err)
 			c.Status(http.StatusInternalServerError)
